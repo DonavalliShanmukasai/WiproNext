65f821b5613a217e018103372d8f9d2bc3ca4430
Donavalli Shanmuka Sai <90787451+DonavalliShanmukasai@users.noreply.github.com>
2023-02-10 09:23:24 +0530

    Create positive, negative or zero.java

---
+++ Day-1/positive, negative or zero.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class tx1 {
+public static void main(String[] args) {
+Scanner sc= new Scanner(System.in);
+System.out.print("Enter the number :");
+int a = sc.nextInt(); if (a>0)
+{
+System.out.print("positive");
+} else if
+(a<0) {
+System.out.print("Negative");
+} else
+System.out.print("zero");
+}
+}
