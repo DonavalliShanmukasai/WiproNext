51412a9a61e7971b8134815fa5fb103dd453a4c3
Donavalli Shanmuka Sai <90787451+DonavalliShanmukasai@users.noreply.github.com>
2023-02-11 11:51:09 +0530

    Update Sum of Sums of Digits in Cyclic order.java

--- Day-6/Sum of Sums of Digits in Cyclic order.java
+++ Day-6/Sum of Sums of Digits in Cyclic order.java
@@ -5,7 +5,15 @@ import  java.util.*;
 class UserMainCode{
 
 public int sumOfSumsOfDigits(int input1){             
+  {
+  String num = String.valueOf(input1);
+        int sum = 0;
 
-String str=Integer.toString(input1); 
-
-int sum=0; 
+        for (int i = 0; i < num.length(); i++) {
+            for (int j = i; j < num.length(); j++) {
+                sum += Integer.parseInt(String.valueOf(num.charAt(j)));
+            }
+        }
+        return sum;
+    }
+}
